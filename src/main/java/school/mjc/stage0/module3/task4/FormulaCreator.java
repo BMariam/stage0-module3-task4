329701d239fe8887889e8ab46bc12d844297946c
BMariam <mariambaghdasaryan77@gmail.com>
2023-07-26 18:20:53 +0400

    Updated files

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+		int a = 0;
+		int b = 0;
+
+		double formula = (9 * Math.pow(a, 2) - 5 * b + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
